b6318ab61df618f44159128f2f0558e3d3795ac2
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-03-11 19:38:17 +0530

    Create_remove_sorted_array
    Python implementation of the Remove Duplicates from Sorted Array problem using the Two Pointer technique with O(n) time complexity and O(1) space complexity.

---
+++ Array/remove_duplicates_sorted_array.py
@@ -0,0 +1,19 @@
+def removeDuplicates(nums):
+
+    i = 0
+
+    for j in range(1, len(nums)):
+
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+
+    return i + 1
+
+
+nums = [1,1,2,2,3]
+
+k = removeDuplicates(nums)
+
+print("Unique count:", k)
+print("Array after removing duplicates:", nums[:k])
