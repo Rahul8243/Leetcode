515e3ccca861844cc1796642b73a97a5340b69b6
Rahul8243 <160226465+Rahul8243@users.noreply.github.com>
2026-03-11 19:41:51 +0530

    longest_Commom_Prefix

---
+++ string/longestCommonPrefix.py
@@ -0,0 +1,20 @@
+def longestCommonPrefix(strs):
+
+    prefix = strs[0]
+
+    for s in strs[1:]:
+
+        i = 0
+
+        while i < len(prefix) and i < len(s) and prefix[i] == s[i]:
+            i += 1
+
+        prefix = prefix[:i]
+
+        if prefix == "":
+            return ""
+
+    return prefix
+
+
+print(longestCommonPrefix(["tarun","tata","tataya"]))
